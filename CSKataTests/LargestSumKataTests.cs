20682fd33f8cda8213898a700af10282274c8fae
Leon Tsai <leontsai218@outlook.com>
2019-03-06 14:20:14 +0800

    Complete Kata No.18.

---
+++ CSKataTests/LargestSumKataTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class LargestSumKataTests
+    {
+        [TestMethod()]
+        public void WithoutNumbers()
+        {
+            var numbers = SetNumbers();
+            LargestSumShouldBe(numbers, 0);
+        }
+
+        [TestMethod()]
+        public void AllNegativeNumbers()
+        {
+            var numbers = SetNumbers(-1, -2, -3);
+            LargestSumShouldBe(numbers, 0);
+        }
+
+        [TestMethod()]
+        public void AllPositiveNumbers()
+        {
+            var numbers = SetNumbers(1, 2, 3);
+            LargestSumShouldBe(numbers, 6);
+        }
+
+        [TestMethod()]
+        public void MixNumbers()
+        {
+            var numbers = SetNumbers(31, -41, 59, 26, -53, 58, 97, -93, -23, 84);
+            LargestSumShouldBe(numbers, 187);
+        }
+
+        private int[] SetNumbers(params int[] numbers)
+        {
+            return numbers;
+        }
+
+        private void LargestSumShouldBe(int[] numbers, int expected)
+        {
+            Assert.AreEqual(expected, LargestSumKata.LargestSum(numbers));
+        }
+    }
+}
---
+++ LargestSumKata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public static class LargestSumKata
+    {
+        public static int LargestSum(IEnumerable<int> numbers)
+        {
+            int maxSum = 0;
+            int sum = 0;
+            foreach (int n in numbers)
+            {
+                sum += n;
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+                else if (sum < 0)
+                {
+                    sum = 0;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
