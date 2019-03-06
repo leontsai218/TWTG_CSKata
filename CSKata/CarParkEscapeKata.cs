e48d18b770a275fb50ab91ceae3d576eb7af20d4
Leon Tsai <leontsai218@outlook.com>
2019-03-06 16:58:33 +0800

    Kata No.19. First version.

---
+++ CSKata/CarParkEscapeKata.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public static class CarParkEscapeKata
+    {
+        public static string[] Escape(int[,] carpark)
+        {
+            var result = new List<string>();
+
+            int currentPosition = -1;
+            for (int i = 0; i < carpark.GetLength(0); i++)
+            {
+                var is1stFloor = Is1stFloor(carpark, i);
+
+                int targetPosition = -1;
+                for (int j = 0; j < carpark.GetLength(1); j++)
+                {
+                    var parkCode = carpark[i, j];
+                    if (parkCode == 1)
+                    {
+                        targetPosition = j;
+                    }
+                    else if (parkCode == 2)
+                    {
+                        currentPosition = j;
+                    }
+
+                    if (is1stFloor)
+                    {
+                        targetPosition = carpark.GetLength(1) - 1;
+                    }
+                }
+
+                if (currentPosition > -1)
+                {
+                    if (currentPosition != targetPosition)
+                    {
+                        string direction = currentPosition > targetPosition ? "L" : "R";
+                        result.Add($"{direction}{Math.Abs(currentPosition - targetPosition)}");
+
+                    }
+
+                    currentPosition = targetPosition;
+                    if (!is1stFloor)
+                    {
+                        int continuousDownCount = 0;
+
+                        while (!Is1stFloor(carpark, i) && carpark[i + 1, currentPosition] == 1)
+                        {
+                            continuousDownCount++;
+                            i += (continuousDownCount);
+                        }
+                        result.Add($"D{continuousDownCount + 1}");
+                    }
+
+                }
+            }
+
+
+            return result.ToArray();
+        }
+
+        private static bool Is1stFloor(int[,] carpark, int i)
+        {
+            return i == carpark.GetLength(0) - 1;
+        }
+    }
+}
---
+++ CSKataTests/CarParkEscapeKataTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class CarParkEscapeKataTests
+    {
+        [TestMethod]
+        public void OneFloorAndParkNextExit()
+        {
+            int[,] carpark = new int[,] { { 2, 0 } };
+            string[] result = new string[] { "R1" };
+            ResultShouldBe(carpark, result);
+        }
+
+        [TestMethod]
+        public void BasicTest1()
+        {
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "L4", "D1", "R4" };
+            ResultShouldBe(carpark, result);
+        }
+
+        [TestMethod]
+        public void BasicTest2()
+        {
+            int[,] carpark = new int[,] { { 2, 0, 0, 1, 0 },
+                                          { 0, 0, 0, 1, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "R3", "D2", "R1" };
+            ResultShouldBe(carpark, result);
+        }
+
+        [TestMethod]
+        public void BasicTest3()
+        {
+            int[,] carpark = new int[,] { { 0, 2, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "R3", "D3" };
+            ResultShouldBe(carpark, result);
+        }
+
+        [TestMethod]
+        public void BasicTest4()
+        {
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 1, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = new string[] { "L4", "D1", "R4", "D1", "L4", "D1", "R4" };
+            ResultShouldBe(carpark, result);
+        }
+
+        [TestMethod]
+        public void BasicTest5()
+        {
+            int[,] carpark = new int[,] { { 0, 0, 0, 0, 2 } };
+            string[] result = new string[] { };
+            ResultShouldBe(carpark, result);
+        }
+
+
+
+        private void ResultShouldBe(int[,] carpark, string[] expected)
+        {
+            string[] actual = CarParkEscapeKata.Escape(carpark);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
