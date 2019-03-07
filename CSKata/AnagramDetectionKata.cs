68ab0e9d08d12d3c8430190511ee47a8fe708c0f
Leon Tsai <leontsai218@outlook.com>
2019-03-07 13:52:58 +0800

    Complete Kata No.20. (First version)

---
+++ CSKata/AnagramDetectionKata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public class AnagramDetectionKata
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            test = test.ToLower();
+            original = original.ToLower();
+
+            var letterPool = new Dictionary<char, int>();
+            foreach(var c in original)
+            {
+                if (letterPool.ContainsKey(c))
+                {
+                    letterPool[c]++;
+                }
+                else
+                {
+                    letterPool.Add(c, 1);
+                }
+            }
+
+            foreach(var c in test)
+            {
+                if (letterPool.ContainsKey(c))
+                {
+                    letterPool[c]--;
+                }
+            }
+
+
+            foreach(var letter in letterPool)
+            {
+                if(letter.Value != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CSKataTests/AnagramDetectionKataTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class AnagramDetectionKataTests
+    {
+        [TestMethod()]
+        public void OneLetterAndSameCase()
+        {
+            string original = "A";
+            string test = "A";
+
+            Assert.AreEqual(true, AnagramDetectionKata.IsAnagram(test, original));
+        }
+
+        [TestMethod()]
+        public void OneLetterAndDiffentCase()
+        {
+            string original = "a";
+            string test = "A";
+
+            Assert.AreEqual(true, AnagramDetectionKata.IsAnagram(test, original));
+        }
+
+        [TestMethod()]
+        public void SameWord()
+        {
+            string original = "coffee";
+            string test = "coffee";
+
+            Assert.AreEqual(true, AnagramDetectionKata.IsAnagram(test, original));
+        }
+
+        [TestMethod()]
+        public void SameLettersSameCount()
+        {
+            string original = "toffee";
+            string test = "foefet";
+
+            Assert.AreEqual(true, AnagramDetectionKata.IsAnagram(test, original));
+        }
+
+        [TestMethod()]
+        public void SameLettersDifferentCount()
+        {
+            string original = "apple";
+            string test = "pale";
+
+            Assert.AreEqual(false, AnagramDetectionKata.IsAnagram(test, original));
+        }
+    }
+}
