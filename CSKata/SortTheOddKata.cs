69779af5399b72e6f5aa67fc4f553e4225e71726
Leon Tsai <leontsai218@outlook.com>
2019-03-07 16:45:25 +0800

    Complete Kata No.21.

---
+++ CSKata/SortTheOddKata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public class SortTheOddKata
+    {
+        public static int[] SortArray(int[] array)
+        {
+            if (array.Length > 0)
+            {
+                var sortedNums = new List<int>();
+                var odds = new Queue<int>(array.Where(x => x % 2 == 1).OrderBy(x => x));
+
+                foreach(int n in array)
+                {
+                    sortedNums.Add(n % 2 == 0 ? n : odds.Dequeue());
+                }
+
+                return sortedNums.ToArray();
+            }
+            return array;
+        }
+    }
+}
---
+++ CSKataTests/SortTheOddKataTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class SortTheOddKataTests
+    {
+        [TestMethod()]
+        public void EmptyArray()
+        {
+            var array = new int[0];
+            var expected = array;
+            SortedResultShouldBe(array, expected);
+        }
+
+        
+
+        [TestMethod()]
+        public void AllOddArray()
+        {
+            var array = new int[] { 5, 9, 7, 1 };
+            var expected = new int[] { 1, 5, 7, 9 };
+            SortedResultShouldBe(array, expected);
+        }
+
+        [TestMethod()]
+        public void AllEvenArray()
+        {
+            var array = new int[] { 6, 4, 2, 8 };
+            var expected = new int[] { 6, 4, 2, 8 };
+            SortedResultShouldBe(array, expected);
+        }
+
+        [TestMethod()]
+        public void NormalArray()
+        {
+            var array = new int[] { 5, 3, 2, 8, 1, 4 };
+            var expected = new int[] { 1, 3, 2, 8, 5, 4 };
+            SortedResultShouldBe(array, expected);
+        }
+
+        [TestMethod()]
+        public void NormalArray02()
+        {
+            var array = new int[] { 5, 3, 1, 8, 0 };
+            var expected = new int[] { 1, 3, 5, 8, 0 };
+            SortedResultShouldBe(array, expected);
+        }
+
+        private void SortedResultShouldBe(int[] array, int[] expected)
+        {
+            CollectionAssert.AreEqual(expected, SortTheOddKata.SortArray(array));
+        }
+    }
+}
