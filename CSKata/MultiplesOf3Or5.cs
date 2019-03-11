cef43e15f15c5073518b80ed5f8573885026cc82
Leon Tsai <leontsai218@outlook.com>
2019-03-11 14:52:47 +0800

    Complete Kata No.25. (Loop version)

---
+++ CSKata/MultiplesOf3Or5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public class MultiplesOf3Or5
+    {
+        public static int Solution(int value)
+        {
+            int sum = 0;
+            int multipleOf3 = 3,
+                multipleOf5 = 5;
+            while (multipleOf3 < value)
+            {
+                while (multipleOf3 < multipleOf5)
+                {
+                    sum += multipleOf3;
+                    multipleOf3 += 3;
+                }
+
+                if (multipleOf5 < value && multipleOf5 % 3 != 0)
+                {
+                    sum += multipleOf5;
+                }
+
+                multipleOf5 += 5;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ CSKataTests/MultiplesOf3Or5Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class MultiplesOf3Or5Tests
+    {
+        [TestMethod()]
+        public void SimpleCase()
+        {
+            ResultShouldBe(10, 23);
+        }
+
+        [TestMethod()]
+        public void Zero()
+        {
+            ResultShouldBe(0, 0);
+        }
+
+        [TestMethod()]
+        public void HaveCommonMultiple()
+        {
+            ResultShouldBe(20, 78);
+        }
+
+        private static void ResultShouldBe(int value, int expected)
+        {
+            Assert.AreEqual(expected, MultiplesOf3Or5.Solution(value));
+        }
+
+
+    }
+}
