4a97dbc2c9d6312da2cda7d9d79ba30441d91575
Leon Tsai <leontsai218@outlook.com>
2019-03-11 15:06:53 +0800

    Update Kata No.25. (Loop version)

--- CSKata/MultiplesOf3Or5.cs
+++ CSKata/MultiplesOf3Or5.cs
@@ -11,22 +11,18 @@ namespace CSKata
         public static int Solution(int value)
         {
             int sum = 0;
-            int multipleOf3 = 3,
-                multipleOf5 = 5;
-            while (multipleOf3 < value)
-            {
-                while (multipleOf3 < multipleOf5)
-                {
-                    sum += multipleOf3;
-                    multipleOf3 += 3;
-                }
 
-                if (multipleOf5 < value && multipleOf5 % 3 != 0)
+            for (int multipleOf3 = 3; multipleOf3 < value; multipleOf3 += 3)
+            {
+                sum += multipleOf3;
+            }
+
+            for (int multipleOf5 = 5; multipleOf5 < value; multipleOf5 += 5)
+            {
+                if (multipleOf5 % 3 != 0)
                 {
                     sum += multipleOf5;
                 }
-
-                multipleOf5 += 5;
             }
 
             return sum;
