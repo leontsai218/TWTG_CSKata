87f1ab736d2dee8b238b34b9ba85a5f79f97292f
Leon Tsai <leontsai218@outlook.com>
2019-03-11 12:07:19 +0800

    Complete Kata No.23.

---
+++ CSKata/PascalsTriangleKata.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public class PascalsTriangleKata
+    {
+        public static List<int> PascalsTriangle(int n)
+        {
+
+            // Build tiers
+            List<Tier> tiers = GetTiers(n);
+
+            // Get Result
+            List<int> result = GetResult(tiers);
+
+            return result;
+        }
+
+        private static List<int> GetResult(List<Tier> tiers)
+        {
+            var result = new List<int>();
+            foreach (Tier tier in tiers)
+            {
+                foreach (int element in tier.Elements)
+                {
+                    result.Add(element);
+                }
+            }
+
+            return result;
+        }
+
+        private static List<Tier> GetTiers(int n)
+        {
+            var tiers = new List<Tier>();
+            for (int levenIndex = 0; levenIndex < n; levenIndex++)
+            {
+                var tier = new Tier(levenIndex + 1, levenIndex == 0 ? null : tiers[levenIndex - 1]);
+                tiers.Add(tier);
+            }
+
+            return tiers;
+        }
+
+        class Tier
+        {
+            private readonly int level;
+
+            public Tier(int level, Tier previousTier)
+            {
+                this.level = level;
+                Elements = GenerateElements(previousTier);
+            }
+
+            private List<int> GenerateElements(Tier previousTier)
+            {
+                var result = new List<int>();
+                for (int i = 0; i < level; i++)
+                {
+                    if (i == 0 || i == level - 1)
+                    {
+                        result.Add(1);
+                    }
+                    else if (i > 0)
+                    {
+                        result.Add(previousTier.Elements[i - 1] + previousTier.Elements[i]);
+                    }
+                }
+
+                return result;
+            }
+
+            public List<int> Elements { get; internal set; }
+        }
+    }
+
+    
+}
---
+++ CSKataTests/PascalsTriangleKataTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class PascalsTriangleKataTests
+    {
+        [TestMethod()]
+        public void SimpleCase_Level1()
+        {
+            ResultShouldBe(1, new int[] { 1 });
+        }
+
+        [TestMethod()]
+        public void SimpleCase_Level2()
+        {
+            ResultShouldBe(2, new int[] { 1, 1, 1 });
+        }
+
+        [TestMethod()]
+        public void SimpleCase_Level3()
+        {
+            ResultShouldBe(3, new int[] { 1, 1, 1, 1, 2, 1 });
+        }
+
+        [TestMethod()]
+        public void AdvanceCase_Level5()
+        {
+            ResultShouldBe(5, new int[] { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1 });
+        }
+
+        [TestMethod()]
+        public void AdvanceCase_Level6()
+        {
+            ResultShouldBe(6, new int[] { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1, 1, 5, 10, 10, 5, 1 });
+        }
+
+        private void ResultShouldBe(int level, int[] expected)
+        {
+            CollectionAssert.AreEqual(expected, PascalsTriangleKata.PascalsTriangle(level));
+        }
+    }
+}
