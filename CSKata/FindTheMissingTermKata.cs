35bf71d3d42ebcbace26c25951a8aa623faf8577
Leon Tsai <leontsai218@outlook.com>
2019-03-11 12:48:43 +0800

    Remove useless code in Kata No.24.

---
+++ CSKata/FindTheMissingTermKata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public class FindTheMissingTermKata
+    {
+        public static int FindMissing(List<int> list)
+        {
+            int CommonDifference = list[1] - list[0];
+            for (int i = 1; i < list.Count - 1; i++)
+            {
+                int diff = list[i + 1] - list[i];
+
+                if (diff < CommonDifference)
+                {
+                    return list[i - 1] + diff;
+                }
+
+                if (diff > CommonDifference)
+                {
+                    return list[i] + CommonDifference;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CSKataTests/FindTheMissingTermKataTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class FindTheMissingTermKataTests
+    {
+        [TestMethod()]
+        public void SimpleCase_7()
+        {
+            var result = FindTheMissingTermKata.FindMissing(new List<int> { 1, 3, 5, 9, 11 });
+            int expected = 7;
+            Assert.AreEqual(7, result);
+        }
+    }
+}
