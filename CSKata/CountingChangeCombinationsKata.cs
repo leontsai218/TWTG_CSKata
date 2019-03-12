105ab8a60feaee72e6a571a09c5962392415e23b
Leon Tsai <leontsai218@outlook.com>
2019-03-12 10:39:05 +0800

    Complete Kata No.22.

---
+++ CSKata/CountingChangeCombinationsKata.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace CSKata
+{
+    public class CountingChangeCombinationsKata
+    {
+        public static int CountCombinations(int money, int[] coins)
+        {
+            var combinationCount = new int[money + 1];
+            combinationCount[0] = 1;
+
+            foreach (int coin in coins)
+            {
+                for (int i = coin; i <= money; i++)
+                {
+                    combinationCount[i] += combinationCount[i - coin];
+                }
+            }
+
+            return combinationCount[money];
+        }
+    }
+}
---
+++ CSKataTests/CountingChangeCombinationsKataTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    //[Ignore]
+    [TestClass()]
+    public class CountingChangeCombinationsKataTests
+    {
+        [TestMethod()]
+        public void NoMoneyAndOneCombination()
+        {
+            ResultShouldBe(1, new int[] { 2 }, 0);
+        }
+        
+
+        [TestMethod()]
+        public void OnlyOneCombination()
+        {
+            ResultShouldBe(1, new int[] { 1 }, 1);
+        }
+
+        [TestMethod()]
+        public void CoinsAreGreaterThanMoneyAndNoCombination()
+        {
+            ResultShouldBe(1, new int[] { 2 }, 0);
+        }
+
+        [TestMethod()]
+        public void CoinsAreLessThanMoneyAndNoCombination()
+        {
+            ResultShouldBe(5, new int[] { 2 }, 0);
+        }
+
+        [TestMethod()]
+        public void TwoCombination()
+        {
+            ResultShouldBe(2, new int[] { 1, 2 }, 2);
+        }
+
+        [TestMethod()]
+        public void SimpleCase()
+        {
+            ResultShouldBe(4, new[] { 1, 2 }, 3);
+        }
+
+        [TestMethod()]
+        public void AnotherSimpleCase()
+        {
+            ResultShouldBe(10, new[] { 5, 2, 3 }, 4);
+        }
+
+        [TestMethod()]
+        public void NoChange()
+        {
+            ResultShouldBe(11, new[] { 5, 7 }, 0);
+        }
+
+        private void ResultShouldBe(int money, int[] coins, int expected)
+        {
+            var result = CountingChangeCombinationsKata.CountCombinations(money, coins);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
