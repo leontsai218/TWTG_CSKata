f801c5cd9abdfd6caa6a64d27f4bc4bb9cdb03a1
Leon Tsai <leontsai218@outlook.com>
2019-03-12 11:05:23 +0800

    Complete Kata No.26.

---
+++ CSKata/ReverseWordsKata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata
+{
+    public class ReverseWordsKata
+    {
+        public static string ReverseWords(string str)
+        {
+            return string.Join(" ", str.Split(' ').Select(w => ReverseWord(w)));
+        }
+
+        private static string ReverseWord(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSKataTests/ReverseWordsKataTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSKata.Tests
+{
+    [TestClass()]
+    public class ReverseWordsKataTests
+    {
+        string OriginalString;
+
+        [TestMethod()]
+        public void OneLetterWord()
+        {
+            GivenString("A");
+            ShouldBe("A");
+        }
+
+        [TestMethod()]
+        public void OneWord()
+        {
+            GivenString("Apple!");
+            ShouldBe("!elppA");
+        }
+
+        [TestMethod()]
+        public void NormalSentence()
+        {
+            GivenString("This is an example!");
+            ShouldBe("sihT si na !elpmaxe");
+        }
+
+        [TestMethod()]
+        public void WithDoubleSpace()
+        {
+            GivenString("double  spaces");
+            ShouldBe("elbuod  secaps");
+        }
+
+        private void GivenString(string str)
+        {
+            OriginalString = str;
+        }
+        private void ShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, ReverseWordsKata.ReverseWords(OriginalString));
+        }
+    }
+}
